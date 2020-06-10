22f99882996799dd38e1731606118249c68543b3
ankit <1994constant@gmail.com>
2020-06-10 23:12:04 +0530

    Add module documentation

--- src/ini.rs
+++ src/ini.rs
@@ -1,10 +1,12 @@
-//!ini
+//!The ini module provides all the functions necessary to load and parse ini-syntax files.
+//!The most important of which is the `Ini` struct.
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::collections::HashMap;
-use std::error::Error;
 
+///A public function of the module to load and parse files into a hashmap.
+///Support for this function will be dropped in the near future and replaced with a macro.
 pub fn load(path: &str) -> HashMap<String, HashMap<String, String>> {
 	let path = Path::new(path);
 	let display = path.display();
@@ -51,25 +53,25 @@ pub fn load(path: &str) -> HashMap<String, HashMap<String, String>> {
 	return map;
 }
 
+///The `Ini` struct simply contains a hashmap of the loaded configuration.
 pub struct Ini {
 	map: HashMap<String, HashMap<String, String>>
 }
 
 impl Ini {
-
+	///Creates a new `HashMap` for the struct and stores the struct in the calling variable.
 	pub fn new() -> Ini {
-		let mut map: HashMap<String, HashMap<String, String>> = HashMap::new();
-		let mut inimap = Ini {
+		let map: HashMap<String, HashMap<String, String>> = HashMap::new();
+		let inimap = Ini {
 			map
 		};
 		inimap
 	}
 
+	///Loads a file from a defined path, parses it and adds the hashmap into our struct.
 	pub fn load(&mut self, path: &str) -> Result<(), String> {
 		let path = Path::new(path);
 		let display = path.display();
-
-		let mut map: HashMap<String, HashMap<String, String>> = HashMap::new();
 
 		let mut file = match File::open(&path) {
 			Err(why) => return Err(format!("couldn't open {}: {}", display, why)),
@@ -77,7 +79,7 @@ impl Ini {
 		};
 
 		let mut s = String::new();
-		self.map = match file.read_to_string(&mut s) {
+		self.map = extend(match file.read_to_string(&mut s) {
 			Err(why) => return Err(format!("couldn't read {}: {}", display, why)),
 			Ok(_) => match self.parse(s) {
 				Err(why) => return Err(why),
@@ -120,6 +122,7 @@ impl Ini {
 		Ok(map)
 	}
 
+	///Returns a clone of the `HashMap` stored in our struct.
 	pub fn get_map(&self) -> Option<HashMap<String, HashMap<String, String>>> {
 		if self.map.is_empty() { None } else { Some(self.map.clone()) }
 	}
