c28679a34ebb657dc0bc5afcf656ab05fa142f36
QEDK <1994constant@gmail.com>
2020-07-19 23:54:52 +0530

    Add tests

--- tests/test.rs
+++ tests/test.rs
@@ -6,8 +6,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 	let mut config = Ini::new();
 	let map = config.load("tests/test.ini")?;
 	config.set_comment_symbols(&[';', '#', '!']);
-	let inpstring = config.read(String::from
-		("defaultvalues=defaultvalues
+	let inpstring = config.read(
+		"defaultvalues=defaultvalues
 		[topsecret]
 		KFC = the secret herb is orega-
 		Empty string =
@@ -21,9 +21,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 		Boolcoerce = 0
 		Int = -31415
 		Uint = 31415
-		Float = 3.1415"))?;
+		Float = 3.1415".to_owned())?;
 	assert_eq!(map, inpstring);
-	config.set("DEFAULT", "defaultvalues", Some(String::from("notdefault")));
+	config.set("DEFAULT", "defaultvalues", Some("notdefault".to_owned()));
 	assert_eq!(config.get("DEFAULT", "defaultvalues").unwrap(), "notdefault");
 	config.setstr("DEFAULT", "defaultvalues", Some("defaultvalues"));
 	assert_eq!(config.get("DEFAULT", "defaultvalues").unwrap(), "defaultvalues");
@@ -52,11 +52,15 @@ fn main() -> Result<(), Box<dyn Error>> {
 	assert_eq!(map["topsecret"]["none string"], None);
 	assert_eq!(map["spacing"]["indented"].clone().unwrap(), "indented");
 	assert_eq!(map["spacing"]["not indented"].clone().unwrap(), "not indented");
+	let mut config2 = config.clone();
+	config2.remove_section("default");
+	assert_eq!(config2.get("default", "nope"), None);
 	let mut_map = config.get_mut_map();
 	mut_map.get_mut("topsecret").unwrap().insert(String::from("none string"), Some(String::from("None string")));
 	assert_eq!(mut_map["topsecret"]["none string"].clone().unwrap(), "None string");
 	mut_map.clear();
-	assert!(config.get_map_ref().is_empty());
+	config2.clear();
+	assert_eq!(config.get_map_ref(), config2.get_map_ref());
 	Ok(())
 }
 
